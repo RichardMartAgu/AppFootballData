f9c6f666f6fcae1bdf7034d8b7fdbd8b7f068e40
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2024-04-02 21:01:09 +0200

    Proyecto base

---
+++ src/main/java/com/svalero/appFootballData/App.java
@@ -0,0 +1,26 @@
+package com.svalero.appFootballData;
+
+import com.svalero.appFootballData.controller.AppController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+        @Override
+        public void start(Stage stage) throws IOException {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com.svalero.appFootballData/view_football_data.fxml"));
+            Scene scene = new Scene(loader.load());
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch();
+        }
+}
